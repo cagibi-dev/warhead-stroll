e1927e3b26790093048b66b01b6537ab6af74264
Paul BLANLOEIL <paul.blanloeil@etu.univ-nantes.fr>
2021-11-15 20:57:58 +0100

    Obfuscate fake passage & fix car not crashing

--- enemies/Car.gd
+++ enemies/Car.gd
@@ -5,7 +5,6 @@ export (Vector2) var vel := Vector2(0, 500)
 
 
 func _physics_process(_delta: float) -> void:
-	var _a = move_and_slide(vel)
 	if position.x > 800:
 		position.x -= 1600
 	if position.y > 600:
@@ -14,8 +13,12 @@ func _physics_process(_delta: float) -> void:
 		position.x += 1600
 	if position.y < -600:
 		position.y += 1200
+	
+	var _a = move_and_slide(vel)
 
 
 func _on_CrashZone_body_entered(body: Node) -> void:
 	if body != self:
 		$CrashZone/Crash.play(0.1)
+		if body is RigidBody2D:
+			body.apply_central_impulse(vel)
